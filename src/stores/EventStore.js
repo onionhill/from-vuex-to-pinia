bf6e82ac0b97bac759dcf5bf7504d29db3103324
Vegard Løkås <vegardlokas@hotmail.com>
2022-07-24 10:31:50 +0200

    merge

--- src/stores/EventStore.js
+++ src/stores/EventStore.js
@@ -11,40 +11,33 @@ export const useEventStore = defineStore('EventStore', {
   getters: {
     numberOfEvents: state => state.events.length
   },
-  actions:{
-    fetchEvents(){
-        return EventService.getEvents()
-            .then(response => {
-                this.events = response.data;
-            })
-            .catch(error => {
-                throw error
-            })
-    },
-    createEvent(event){
-        return EventService.getEvents()
-        .then(response => {
-            this.events.push(event)
+  actions: {
+    createEvent(event) {
+      return EventService.postEvent(event)
+        .then(() => {
+          this.events.push(event)
         })
         .catch(error => {
           throw error
         })
     },
-    fetchEvent(id){
-        const existingEvent = this.events.find(event => event.id === id);
-        if(existingEvent){
-            this.event = existingEvent;
-
-        }else{
-            return EventService.getEvent(id)
-          .then(response => {
-            this.event = response.data;
-          })
-          .catch(error => {
-            throw error
-          })
-        }
+    fetchEvent(id) {
+      return EventService.getEvent(id)
+        .then(response => {
+          this.event = response.data
+        })
+        .catch(error => {
+          throw error
+        })
+    },
+    fetchEvents() {
+      return EventService.getEvents()
+        .then(response => {
+          this.events = response.data
+        })
+        .catch(error => {
+          throw error
+        })
     }
-   
   }
 })
